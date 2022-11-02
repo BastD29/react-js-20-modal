ba469d019492a692aa2a332c4b7d4379dd763930
Bastien <bastien@MBP-di-Bastien.homenet.telecomitalia.it>
2022-11-02 12:33:11 +0100

    second commit

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,20 +1,17 @@
 import './modal.css';
 
-export default function Modal({ handleClose, show, children }) {
+export default function Modal({ handleClose, show }) {
 
     const showHideClassName = show ? "modal display-block" : "modal display-none";
 
     return (
         <div className={showHideClassName}>
-            <div>
-                {children}
                 <button
                     type='button'
                     onClick={handleClose}
                 >
-                    X Close modal
+                    Close modal
                 </button>
-            </div>
         </div>
     )
 }
